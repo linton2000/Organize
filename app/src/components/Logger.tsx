ebd17b4306667f63d2c6f4dba2a200ae01f052d1
Linton Charles <lintoncharles2000@gmail.com>
2022-06-04 15:04:10 +1000

    feat: Start Logger Implementation

---
+++ app/src/components/Logger.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Logger extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
