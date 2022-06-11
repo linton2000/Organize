649d0fb630912114c712825a30fe52b270222da9
Linton Charles <lintoncharles2000@gmail.com>
2022-06-11 17:28:03 +1000

    feat: LogTable UI Prototype

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,18 +1,18 @@
 import { Grid } from "@mui/material";
 import { ReactElement } from "react";
 import Logger from "./components/Logger";
+import LogTable from "./components/LogTable";
 
 function App() {
     let mainGrid: ReactElement = (
         <Grid container spacing={20}>
             <Grid item xs={12} />
-            <Grid item xs={12} />
             <Grid item xs={2} />
             <Grid container item xs={3}>
                 <Logger />
             </Grid>
-            <Grid item xs={2} />
-            <Grid container item xs={5}>
+            <Grid container item xs={6}>
+                <LogTable />
             </Grid>
         </Grid>
     );
