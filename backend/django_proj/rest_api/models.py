79c4a23a172b353153f1a1e37c69e1b27a0355ef
Linton Charles <lintoncharles2000@gmail.com>
2022-09-19 11:54:08 +1000

    feat: Add Session model to django proj

--- backend/django_proj/rest_api/models.py
+++ backend/django_proj/rest_api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Session(models.Model):
+    sessionId = models.AutoField(primary_key=True)
+    startDate = models.DateTimeField()
+    endDate = models.DateTimeField()
+    subject = models.CharField(max_length=30)
