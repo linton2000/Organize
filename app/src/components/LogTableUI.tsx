2f0bbff0f2cf7504dbaf6f2e41d3735d7d2e3902
Linton Charles <lintoncharles2000@gmail.com>
2022-10-24 23:34:29 +1100

    feat: Extract LogTable UI to a separate component

---
+++ app/src/components/LogTableUI.tsx
@@ -0,0 +1,140 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableFooter from "@mui/material/TableFooter";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import TableHead from "@mui/material/TableHead";
+import TablePaginationActions from "./TablePaginationActions";
+
+import { Row } from "./component_types";
+
+export default function LogTableUI(props: any) {
+    const rows: Row[] = props.rows as Row[];
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+    // Avoid a layout jump when reaching the last page with empty rows.
+    const emptyRows =
+        page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+    const handleChangePage = (
+        event: React.MouseEvent<HTMLButtonElement> | null,
+        newPage: number
+    ) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (
+        event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    ) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    const StyledTableCell = styled(TableCell)(({ theme }) => ({
+        [`&.${tableCellClasses.head}`]: {
+            backgroundColor: theme.palette.common.black,
+            color: theme.palette.common.white,
+        },
+        [`&.${tableCellClasses.body}`]: {
+            fontSize: 14,
+        },
+    }));
+
+    const StyledTableRow = styled(TableRow)(({ theme }) => ({
+        "&:nth-of-type(odd)": {
+            backgroundColor: theme.palette.action.hover,
+        },
+        // hide last border
+        "&:last-child td, &:last-child th": {
+            border: 0,
+        },
+    }));
+
+    return (
+        <TableContainer component={Paper}>
+            <Table
+                sx={{ minWidth: 500, minHeight: 473 }}
+                aria-label="custom pagination table"
+            >
+                <TableHead>
+                    <StyledTableRow>
+                        <StyledTableCell>Name</StyledTableCell>
+                        <StyledTableCell align="center">
+                            Start Time
+                        </StyledTableCell>
+                        <StyledTableCell align="center">
+                            Duration
+                        </StyledTableCell>
+                        <StyledTableCell align="center">
+                            End Time
+                        </StyledTableCell>
+                    </StyledTableRow>
+                </TableHead>
+                <TableBody>
+                    {(rowsPerPage > 0
+                        ? rows.slice(
+                              page * rowsPerPage,
+                              page * rowsPerPage + rowsPerPage
+                          )
+                        : rows
+                    ).map((row) => (
+                        <StyledTableRow>
+                            <TableCell
+                                component="th"
+                                style={{ width: 50 }}
+                                scope="row"
+                            >
+                                {row.name}
+                            </TableCell>
+                            <TableCell style={{ width: 200 }} align="center">
+                                {row.startTime}
+                            </TableCell>
+                            <TableCell style={{ width: 160 }} align="center">
+                                {row.duration}
+                            </TableCell>
+                            <TableCell style={{ width: 200 }} align="center">
+                                {row.endTime}
+                            </TableCell>
+                        </StyledTableRow>
+                    ))}
+                    {emptyRows > 0 && (
+                        <StyledTableRow style={{ height: 53 * emptyRows }}>
+                            <TableCell colSpan={6} />
+                        </StyledTableRow>
+                    )}
+                </TableBody>
+                <TableFooter>
+                    <TableRow>
+                        <TablePagination
+                            rowsPerPageOptions={[
+                                5,
+                                10,
+                                25,
+                                { label: "All", value: -1 },
+                            ]}
+                            colSpan={3}
+                            count={rows.length}
+                            rowsPerPage={rowsPerPage}
+                            page={page}
+                            SelectProps={{
+                                inputProps: {
+                                    "aria-label": "rows per page",
+                                },
+                                native: true,
+                            }}
+                            onPageChange={handleChangePage}
+                            onRowsPerPageChange={handleChangeRowsPerPage}
+                            ActionsComponent={TablePaginationActions}
+                        />
+                    </TableRow>
+                </TableFooter>
+            </Table>
+        </TableContainer>
+    );
+}
