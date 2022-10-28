cdfc108b04bd7602ad514a2d5a265b53c9156b69
Linton Charles <lintoncharles2000@gmail.com>
2022-10-28 20:00:58 +1100

    feat: Implement duration calc

--- app/src/components/utils.ts
+++ app/src/components/utils.ts
@@ -1,10 +1,32 @@
 export function formatSessionDate(dateParam: Date): string {
     let date: Date = new Date(dateParam);
-    return `${date.toLocaleTimeString('en-AU')}, ${date.toLocaleDateString('en-AU')}`;
+    return `${date.toLocaleTimeString("en-AU")}, ${date.toLocaleDateString(
+        "en-AU"
+    )}`;
 }
 
 //TODO
-export function calcDuration(startDate: Date, endDate: Date): string{
-    let duration: string = " ";
+export function calcDuration(startDateParam: Date, endDateParam: Date): string {
+    let duration: string;
+    let startDate: Date = new Date(startDateParam);
+    let endDate: Date = new Date(endDateParam);
+    let intDuration: number = (endDate.getTime() - startDate.getTime()) / 1000;
+    let hours, minutes, seconds: number;
+
+    intDuration = Math.round(intDuration);
+    if (intDuration < 60) {
+        duration = `${intDuration.toString()} secs`;
+    } else if (intDuration > 60 && intDuration < 3600) {
+        minutes = Math.round(intDuration / 60);
+        seconds = Math.round(intDuration % 60);
+        duration = `${minutes} mins ${seconds} secs`;
+    } else if (intDuration > 3600 && intDuration < 1e6) {
+        hours = Math.round(intDuration / 3600);
+        minutes = Math.round((intDuration % 3600) / 60);
+        seconds = Math.round(intDuration % 60);
+        duration = `${hours} hrs ${minutes} mins ${seconds} secs`;
+    } else {
+        duration = "Error Duration!";
+    }
     return duration;
 }
