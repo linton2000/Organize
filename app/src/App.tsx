11e006d5511a378f0a8400ed800a52b2fa14af43
Linton Charles <lintoncharles2000@gmail.com>
2022-10-28 21:32:51 +1100

    feat: Add application title

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,24 +1,34 @@
 import { Grid } from "@mui/material";
-import { useState, ReactElement } from "react";
+import { useState, useEffect, ReactElement } from "react";
 import Logger from "./app/Logger";
 import LogTable from "./components/LogTable";
 
 function App() {
     const [rerender, setRerender] = useState<boolean>();
 
-    if(rerender == undefined){
+    if (rerender == undefined) {
         setRerender(false);
     }
+
+    useEffect(() => {
+        document.title = "Organize!";
+    });
 
     let mainGrid: ReactElement = (
         <Grid container spacing={20}>
             <Grid item xs={12} />
             <Grid item xs={1} />
             <Grid container item xs={3}>
-                <Logger rerender={rerender as boolean} setRerender={setRerender}/>
+                <Logger
+                    rerender={rerender as boolean}
+                    setRerender={setRerender}
+                />
             </Grid>
             <Grid container item xs={7}>
-                <LogTable rerender={rerender as boolean} setRerender={setRerender}/>
+                <LogTable
+                    rerender={rerender as boolean}
+                    setRerender={setRerender}
+                />
             </Grid>
         </Grid>
     );
