56142afd089c3de5d929572da355b3adcef0b41c
Linton Charles <lintoncharles2000@gmail.com>
2022-11-11 14:23:57 +1100

    style: Increase AppBar text size

--- app/src/routes/Layout.tsx
+++ app/src/routes/Layout.tsx
@@ -12,7 +12,7 @@ export default function Layout() {
       <CssBaseline />
       <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
         <Toolbar>
-          <Typography variant="h6" noWrap component="div">
+          <Typography variant="h4" noWrap component="div">
             Organize
           </Typography>
         </Toolbar>
