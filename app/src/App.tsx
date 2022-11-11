b6ee5f41b3e6139af36b903dc661db4c0717d6c4
Linton Charles <lintoncharles2000@gmail.com>
2022-11-11 13:22:32 +1100

    feat: Extract Home component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,39 +1,7 @@
-import { Grid } from "@mui/material";
-import { useState, useEffect, ReactElement } from "react";
-import Logger from "./app/Logger";
-import LogTable from "./components/LogTable";
+import Home from "routes/Home";
 
 function App() {
-    const [rerender, setRerender] = useState<boolean>();
-
-    if (rerender == undefined) {
-        setRerender(false);
-    }
-
-    useEffect(() => {
-        document.title = "Organize!";
-    });
-
-    let mainGrid: ReactElement = (
-        <Grid container spacing={20}>
-            <Grid item xs={12} />
-            <Grid item xs={1} />
-            <Grid container item xs={3}>
-                <Logger
-                    rerender={rerender as boolean}
-                    setRerender={setRerender}
-                />
-            </Grid>
-            <Grid container item xs={7}>
-                <LogTable
-                    rerender={rerender as boolean}
-                    setRerender={setRerender}
-                />
-            </Grid>
-        </Grid>
-    );
-
-    return mainGrid;
+    return <Home/>;
 }
 
 export default App;
---
+++ app/src/routes/Home.tsx
@@ -0,0 +1,39 @@
+import { Grid } from "@mui/material";
+import { useState, useEffect, ReactElement } from "react";
+import Logger from "app/Logger";
+import LogTable from "components/LogTable";
+
+function Home(){
+    const [rerender, setRerender] = useState<boolean>();
+
+    if (rerender == undefined) {
+        setRerender(false);
+    }
+
+    useEffect(() => {
+        document.title = "Organize!";
+    });
+
+    let mainGrid: ReactElement = (
+        <Grid container spacing={20}>
+            <Grid item xs={12} />
+            <Grid item xs={1} />
+            <Grid container item xs={3}>
+                <Logger
+                    rerender={rerender as boolean}
+                    setRerender={setRerender}
+                />
+            </Grid>
+            <Grid container item xs={7}>
+                <LogTable
+                    rerender={rerender as boolean}
+                    setRerender={setRerender}
+                />
+            </Grid>
+        </Grid>
+    );
+
+    return mainGrid;
+}
+
+export default Home;
