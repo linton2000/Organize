78f71a0f2d2964f79471ed4dd84a4b435a150e78
Linton Charles <lintoncharles2000@gmail.com>
2022-12-13 18:30:29 +1100

    feat: Implement Layout element

--- app/src/routes/Layout.tsx
+++ app/src/routes/Layout.tsx
@@ -1,27 +1,36 @@
-import { Outlet, Link } from "react-router-dom";
-import Box from '@mui/material/Box';
-import AppBar from '@mui/material/AppBar';
-import CssBaseline from '@mui/material/CssBaseline';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import PermenantDrawer from "components/PermenantDrawer";
+import { Outlet } from "react-router-dom";
+import Box from "@mui/material/Box";
+import AppBar from "@mui/material/AppBar";
+import CssBaseline from "@mui/material/CssBaseline";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import DefaultDrawer from "components/DefaultDrawer";
 
 export default function Layout() {
-  return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-        <Toolbar>
-          <Typography variant="h4" noWrap component="div">
-            Organize
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <PermenantDrawer navItems={['Home', 'Schedule', 'Subject Management', 'Analytics']}/>
-      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        <Toolbar />
-        <Outlet />
-      </Box>
-    </Box>
-  );
+    let navItemsList: Array<Array<string>> = [
+        ["Home", "/"],
+        ["Schedule", "/schedule"],
+        ["Subject Management", "/subject-management"],
+        ["Analytics", "/analytics"]
+    ];
+
+    return (
+        <Box sx={{ display: "flex" }}>
+            <CssBaseline />
+            <AppBar
+                position="fixed"
+                sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+            >
+                <Toolbar>
+                    <Typography variant="h4" noWrap component="div">
+                        Organize
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <DefaultDrawer navItems={navItemsList} />
+            <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+                <Outlet />
+            </Box>
+        </Box>
+    );
 }
