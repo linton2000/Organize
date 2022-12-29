16a9806753ae47e4969b0aef6beeff859d6323c6
Linton Charles <lintoncharles2000@gmail.com>
2022-12-29 17:38:16 +1100

    style: Center LogTable

--- app/src/routes/Analytics.tsx
+++ app/src/routes/Analytics.tsx
@@ -4,8 +4,7 @@ import LogTable from "components/LogTable/LogTable";
 
 export default function Analytics() {
     let mainGrid: ReactElement = (
-        <Grid container spacing={20}>
-            <Grid item xs={12} />
+        <Grid container spacing={10} sx={{marginTop: 0.5}}>
             <Grid item xs={1} />
             <Grid item xs={10}>
                 <LogTable />
