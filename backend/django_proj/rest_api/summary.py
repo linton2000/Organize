30de433a171a0cae83a915a8fd7999fe51291fb0
Linton Charles <lintoncharles2000@gmail.com>
2025-02-11 19:54:57 +0530

    Remove rest_api app

--- backend/django_proj/rest_api/summary.py
+++
@@ -1,8 +0,0 @@
-from .models import Session
-
-def computeSummary():
-	edt = Session.objects.latest('endDate').endDate
-	ts = int(round(edt.timestamp()))
-
-	summary = {'lastWorked': ts}
-	return summary
