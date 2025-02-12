e0d14b38971578ec272fc16698a8e20d33f1c5a1
Linton Charles <lintoncharles2000@gmail.com>
2025-02-12 09:25:49 +0530

    Add Session model

--- backend/src/organize/models.py
+++ backend/src/organize/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Session(models.Model):
+    sessionId = models.BigAutoField(primary_key=True)
+    start = models.DateTimeField(auto_now_add=True)
+    end = models.DateTimeField()
+    subject = models.CharField(max_length=30)
+
+    def __str__(self) -> str:
+        return f"Session ID: {self.sessionId}"
+    
