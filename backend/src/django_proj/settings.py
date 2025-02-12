1596e355f71ec1acf4b93c942a00812aae92613f
Linton Charles <lintoncharles2000@gmail.com>
2025-02-12 09:05:02 +0530

    Remove rest_api from INSTALLED_APPS

--- backend/src/django_proj/settings.py
+++ backend/src/django_proj/settings.py
@@ -41,8 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'corsheaders',
-    'rest_api'
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
