ff167fc32390ddee53be41f0a2a2937034292202
Linton Charles <lintoncharles2000@gmail.com>
2025-08-22 12:56:31 +1000

    Fix session model attributes to match frontend & db

--- backend/src/organize/models.py
+++ backend/src/organize/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Session(models.Model):
     sessionId = models.BigAutoField(primary_key=True)
-    start = models.DateTimeField(auto_now_add=True)
-    end = models.DateTimeField()
+    startDate = models.DateTimeField(auto_now_add=True)
+    endDate = models.DateTimeField()
     subject = models.CharField(max_length=30)
 
     def __str__(self) -> str:
