1e5a2e9c2a7bf45cd261fa3caa55108e31c73462
Linton Charles <lintoncharles2000@gmail.com>
2025-08-22 14:30:21 +1000

    Fix session url in frontend

--- app/src/scripts/constants.ts
+++ app/src/scripts/constants.ts
@@ -1,2 +1,2 @@
-export const SESSION_URL: string = "http://127.0.0.1:8000/api/session/";
+export const SESSION_URL: string = "http://127.0.0.1:8000/api/sessions/";
 export const SUMMARY_URL: string = "http://127.0.0.1:8000/api/summary/";
