c414171db6b5d29d1136e1aa2a20fd2b5cfe8a47
Linton Charles <lintoncharles2000@gmail.com>
2025-10-02 10:47:56 +1000

    Add newly implemented API URLs to React app

--- app/src/scripts/constants.ts
+++ app/src/scripts/constants.ts
@@ -1,2 +1,6 @@
-export const SESSION_URL: string = "http://127.0.0.1:8000/api/sessions/";
-export const SUMMARY_URL: string = "http://127.0.0.1:8000/api/summary/";
+export const API_ROOT_URL: string = "http://127.0.0.1:8000/api/";
+export const SUMMARY_URL: string = API_ROOT_URL + "summary/";
+export const SESSION_URL: string = API_ROOT_URL + "session/";
+export const START_SESSION_URL: string = API_ROOT_URL + "start-session/";
+export const ACTIVE_SESSION_URL: string = API_ROOT_URL + "active-session/";
+export const END_SESSION_URL: string = API_ROOT_URL + "end-session/";
