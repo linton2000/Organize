4ad482aabb805e18ebb361024aa962dbfd9fe04f
Linton Charles <lintoncharles2000@gmail.com>
2025-10-04 17:02:30 +1000

    Refactor Logger to be a function component & to use new api session methods

--- app/src/components/Logger/Logger.tsx
+++ app/src/components/Logger/Logger.tsx
@@ -1,13 +1,9 @@
+import { useState } from "react";
 import { Button, Stack } from "@mui/material";
-import { Component, ReactElement } from "react";
-import Session from "../../domain/Session";
+
+import { startSession, getActiveSession, endSession } from "scripts/api_methods";
 import Timer from "./Timer";
 import SubjectSelector from "./SubjectSelector";
-
-interface LoggerState {
-    isLogging: boolean;
-    subject: string;
-}
 
 interface LoggerProps {
     rerender: boolean;
@@ -16,108 +12,48 @@ interface LoggerProps {
 
 /** This component handles log controlling logic and acts as an interface between
  * the logger's UI and the business logic classes */
-export default class Logger extends Component<LoggerProps, LoggerState> {
-    session: Session | null;
+export default function Logger(props: LoggerProps) {
+    const [startDate, setStartDate] = useState<string | null>(null);
+    const [subject, setSubject] = useState<string>("");
 
-    constructor(props: LoggerProps) {
-        super(props);
-        this.session = null;
-        this.state = { isLogging: false, subject: "" };
-        this.startTimer = this.startTimer.bind(this);
-        this.endTimer = this.endTimer.bind(this);
+    // Logger configured according to 3 states - Currently Logging, Subject Selected or Nothing Selected.
+    const isSubjectSelectionDisabled: boolean = startDate != null;
+    const timerButtonColor = startDate ? "warning" : "success";
+    const timerButtonText: string = startDate ? "End Session" : "Start Session";
+    const isTimerDisabled: boolean = subject == "" ? true : false;
+
+    function endTimer() {
+        endSession();
+        setSubject("");
+        setStartDate(null);
     }
 
-    startTimer() {
-        if (this.state.subject != "") {
-            this.session = new Session(new Date(), this.state.subject);
-            this.setState({ isLogging: true });
+    async function startTimer() {
+        if (subject != "") {
+            const session = await startSession(subject);    // Potential optimisation: Use a local date without waiting & reconcile later
+            setStartDate(session.startDate);
+        }
+        else {
+            console.error("startTimer() called without a subject.");
         }
     }
 
-    endTimer() {
-        if (this.session != null) this.session.end(); // API POST request made here
-        this.setState({ isLogging: false });
-        this.props.setRerender(true);
-    }
-
-    render() {
-        let res: ReactElement;
-        // User has selected a subject & started the timer
-        if (this.state.isLogging) {
-            res = (
-                <Stack spacing={3}>
-                    <Timer startTime={new Date()} />
-                    <Button
-                        variant="outlined"
-                        color="warning"
-                        onClick={this.endTimer}
-                    >
-                        End Session
-                    </Button>
-                    <SubjectSelector
-                        subject={this.state.subject}
-                        isDisabled={true}
-                        onSubjectChange={(value) =>
-                            this.setState({ subject: value })
-                        }
-                    />
-                </Stack>
-            );
-        // User has selected a subject but hasn't started the timer
-        } else if (this.state.subject) {
-            res = (
-                <Stack spacing={3}>
-                    <Stack spacing={1}>
-                        <h1 style={{ fontFamily: "monospace" }}>00:00:00</h1>
-                        <p style={{ fontFamily: "Georgia, serif" }}>
-                            No better time than the present...
-                        </p>
-                    </Stack>
-                    <Button
-                        variant="outlined"
-                        color="success"
-                        onClick={this.startTimer}
-                        disabled={false}
-                    >
-                        Start Session
-                    </Button>
-                    <SubjectSelector
-                        subject={this.state.subject}
-                        isDisabled={false}
-                        onSubjectChange={(value) =>
-                            this.setState({ subject: value })
-                        }
-                    />
-                </Stack>
-            );
-        // User hasn't selected a subject & hasn't started the timer
-        } else {
-            res = (
-                <Stack spacing={3}>
-                    <Stack spacing={1}>
-                        <h1 style={{ fontFamily: "monospace" }}>00:00:00</h1>
-                        <p style={{ fontFamily: "Georgia, serif" }}>
-                            No better time than the present...
-                        </p>
-                    </Stack>
-                    <Button
-                        variant="outlined"
-                        color="success"
-                        onClick={this.startTimer}
-                        disabled={true}
-                    >
-                        Start Session
-                    </Button>
-                    <SubjectSelector
-                        subject={this.state.subject}
-                        isDisabled={false}
-                        onSubjectChange={(value) =>
-                            this.setState({ subject: value })
-                        }
-                    />
-                </Stack>
-            );
-        }
-        return res;
-    }
+    return (
+        <Stack spacing={3}>
+            <Timer startTime={ startDate } />
+            <Button
+                variant="outlined"
+                color={ timerButtonColor }
+                onClick={ startDate ? endTimer : startTimer }
+                disabled={ isTimerDisabled }
+            >
+                { timerButtonText }
+            </Button>
+            <SubjectSelector
+                subject={ subject }
+                isDisabled={ isSubjectSelectionDisabled }
+                onSubjectChange={ setSubject }
+            />
+        </Stack>
+    );
 }
