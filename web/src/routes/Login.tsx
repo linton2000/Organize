01a99a02b5895222149b0fbb8e5a8780b6792e7b
Linton Charles <lintoncharles2000@gmail.com>
2025-11-02 19:17:57 +1100

    Add a simple Login UI component

--- web/src/routes/Login.tsx
+++ web/src/routes/Login.tsx
@@ -1,16 +1,38 @@
 import { Grid } from "@mui/material";
 import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+
 
 export default function Login() {
     return (
-        <Grid container spacing={0} columns={12}>
-            <Grid item xs={4}/>
-            <Grid item xs={4}>                    
-                <Typography>
-                    Login to your Account
-                </Typography>
+        <Grid
+        container
+        columns={12}
+        spacing={0}
+        sx={{ minHeight: '100vh' }}
+        alignItems="center"
+        justifyContent="center"
+        >
+            <Grid item xs={2}>
+                <Stack 
+                    spacing={3} 
+                    sx={{
+                        border: '1px solid',
+                        borderColor: 'divider',   // theme-aware neutral color
+                        borderRadius: 2,          // theme.spacing(2) radius
+                        padding: 4,               // padding inside the outline
+                        backgroundColor: 'background.paper',
+                    }}>
+                    <Typography variant="h5" align="center">
+                        Login to your Account
+                    </Typography>
+                    <TextField required id="username-input" label="Username" variant="outlined"/>
+                    <TextField required type="password" id="password-input" label="Password" variant="outlined"/>
+                    <Button variant="contained"> Submit </Button>
+                </Stack>
             </Grid>
-            <Grid item xs={4}/>
         </Grid>
     );
 }
