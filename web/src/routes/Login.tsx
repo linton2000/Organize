867b946e9aa68d5bf9018e992c41f5d47070a5e1
Linton Charles <lintoncharles2000@gmail.com>
2025-11-03 12:03:52 +1100

    Implement Login page

--- web/src/routes/Login.tsx
+++ web/src/routes/Login.tsx
@@ -1,11 +1,38 @@
+import { FormEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { Grid } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 
+import { useAuth } from "providers/AuthProvider";
+
 
 export default function Login() {
+    const [username, setUsername] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [isSubmitting, setIsSubmitting] = useState<boolean>(false);   // To prevent resubmits while waiting
+    const auth = useAuth();
+    const navigate = useNavigate();
+
+    const handleLogin = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault(); // Stops built-in browser form submission behaviour
+        if (isSubmitting || !username.trim() || !password.trim()) {
+            return;
+        }
+
+        setIsSubmitting(true);
+        try {
+            await auth.login(username, password);
+            navigate('home');   // This is skipped if auth.login throws (eg. due to 401 for invalid creds)
+        } finally {
+            setIsSubmitting(false);
+        }
+    };
+
+    const isDisabled = isSubmitting || !username.trim() || !password.trim();
+
     return (
         <Grid
         container
@@ -15,9 +42,11 @@ export default function Login() {
         alignItems="center"
         justifyContent="center"
         >
-            <Grid item xs={2}>
-                <Stack 
-                    spacing={3} 
+            <Grid item xs={4}>
+                <Stack
+                    component="form"
+                    onSubmit={handleLogin}
+                    spacing={3}
                     sx={{
                         border: '1px solid',
                         borderColor: 'divider',   // theme-aware neutral color
@@ -28,9 +57,30 @@ export default function Login() {
                     <Typography variant="h5" align="center">
                         Login to your Account
                     </Typography>
-                    <TextField required id="username-input" label="Username" variant="outlined"/>
-                    <TextField required type="password" id="password-input" label="Password" variant="outlined"/>
-                    <Button variant="contained"> Submit </Button>
+                    <TextField
+                        required
+                        id="username-input"
+                        label="Username"
+                        variant="outlined"
+                        value={username}
+                        onChange={(event) => setUsername(event.target.value)}
+                    />
+                    <TextField
+                        required
+                        type="password"
+                        id="password-input"
+                        label="Password"
+                        variant="outlined"
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                    />
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        disabled={isDisabled}
+                    >
+                        Submit
+                    </Button>
                 </Stack>
             </Grid>
         </Grid>
