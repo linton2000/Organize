438fff749412baff6b21a245e3acfe5c700b271c
Linton Charles <lintoncharles2000@gmail.com>
2025-11-06 15:19:01 +1100

    Add absolute paths to tsconfig and fix vite config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,16 +1,9 @@
-import { fileURLToPath, URL } from "node:url";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import path from "path";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '/src');
-    },
-  },
   server: {
     port: 3000,
     strictPort: true,
