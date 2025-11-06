9786a061ffc2b4ead4c131654d484f4d2594df3f
Linton Charles <lintoncharles2000@gmail.com>
2025-11-06 14:21:43 +1100

    Add src as base path to Vite & TS config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,9 +1,14 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      src: "/src",
+    },
+  },
   server: {
     port: 3000,
     strictPort: true,
